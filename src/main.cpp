008041c49b0b07bee48ffd38b6a1a5849ffcd2cb
amayaleo <amayaleod@yahoo.com>
2020-07-08 00:48:52 -0300

    master

--- src/main.cpp
+++ src/main.cpp
@@ -20,44 +20,9 @@
 #include <iostream>
 #include "run.h"
 
-
-
-struct B //: A
-{
-    virtual void f()=0;
-    virtual  ~B()=default;
-            
-    /* data */
-};
-
-struct A : public B
-{
-    A(std::string txt)
-    :text(txt)
-    {
-
-    }
-     void f()override; 
-
-    private:
-        std::string text;
-};
-
-void A::f(){
-    std::cout << "/* message */" << text << std::endl;
-
-}
-
-
-
 int main(int argc,char *argv[])
 {
   RUN::App();
 
-  B * b;
-  b=new A(" funcion virtual creada \n");
-  b->f();
-  if(b)  delete b;
-           
     return 0;
 }
